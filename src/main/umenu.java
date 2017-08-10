10f4a8cd298208d1685c30f27a1f3c25bac437e0
Dariusz Jerzewski <d.jerzewski@kainos.com>
2017-08-10 14:35:34 +0100

    Changes to menu

--- src/main/umenu.java
+++ src/main/umenu.java
@@ -1,4 +1,4 @@
-package main;
+package Main;
 
 
 import java.sql.ResultSet;
@@ -20,6 +20,16 @@ public class umenu {
 
 
       while (running) {
+          System.out.println("********************************************************");
+          System.out.println("--------------------Login--------------------");
+          System.out.print("Username: ");
+          String username = sc.next();
+
+          if (username != null)
+              System.out.print("Password: ");
+          String password = sc.next();
+
+          System.out.println("\n");
 
         System.out.println("#########################################################");
         System.out.println("             MAIN MENU");
@@ -43,7 +53,7 @@ public class umenu {
             u.addPerson();
           } else if(answer.equals("2")) {
             try {
-              ResultSet rs = DBConnect.ExecuteQuery("Select * from hr_view limit 50", "hr", "thursday");
+              ResultSet rs = DBConnect.ExecuteQuery("Select * from hr_view limit 50", username, password);
               while(rs.next()){
                 System.out.printf("Employee Number: %s | Name: %s | Address: %s | NIN: %s | Bank: %s | Account Number: %s | Salary: %s | Department: %s\n",
                         rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
@@ -58,7 +68,19 @@ public class umenu {
           } else if(answer.equals("3")){
             //    departmentReport
           } else if (answer.equals("4")){
-
+              try {
+                  ResultSet rs = DBConnect.ExecuteQuery("Select * from hr_view limit 50", username, password);
+                  while(rs.next()){
+                      System.out.printf("Employee Number: %s | Name: %s | Address: %s | NIN: %s | Bank: %s | Account Number: %s | Salary: %s | Department: %s\n",
+                              rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
+                              rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8),
+                              rs.getString(9));
+                  }
+              } catch (ClassNotFoundException e) {
+                  e.printStackTrace();
+              } catch (SQLException e) {
+                  e.printStackTrace();
+              }
           } else if (answer.equals("5")){
 
           } else if (answer.equals("6")){
--- src/main/userStory.java
+++ src/main/userStory.java
@@ -1,4 +1,4 @@
-package main;
+package Main;
 import java.sql.SQLException;
 import java.util.Scanner;
 import java.util.ArrayList;
