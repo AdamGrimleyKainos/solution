567b3686f442bc4679b78bf803d70b65cc0c00d4
AdamGrimleyKainos <a.grimley@kainos.com>
2017-08-10 14:37:28 +0100

    added US3 frame

--- src/main/userStory.java
+++ src/main/userStory.java
@@ -37,8 +37,6 @@ public class userStory {
 
 
   public void addPerson() {
-
-
     System.out.println("Forename: ");
     forename = sc.nextLine();
 
@@ -51,14 +49,14 @@ public class userStory {
     System.out.println("House Number: ");
     houseNumber = sc.nextLine();
 
-    System.out.println("Bank Number: ");
-    bankNum = sc.nextLine();
-
     System.out.println("Post Code: ");
     postCode = sc.nextLine();
 
     System.out.println("NIN: ");
     NIN = sc.nextLine();
+
+    System.out.println("Bank Number: ");
+    bankNum = sc.nextLine();
 
     System.out.println("Bank Name: ");
     bankName = sc.nextLine();
@@ -66,6 +64,62 @@ public class userStory {
     System.out.println("Starting Salary (Do not include decimal point but include the values after the decimal point: ");
     startingSalary = sc.nextInt();
 
+
+    System.out.println("Employee Number: ");
+    employeeNumber = sc.nextInt();
+
+    try {
+      DBConnect.InsertQuery("insert into employees (emp_no, birth_date, first_name, last_name, gender, NIN, house_number, street_name, postcode, hire_date) " +
+                      "VALUES ("+employeeNumber+",NOW(),'"+forename+"', '"+surname+"', '"+"F', '"+NIN+"', '"+houseNumber+"', '"+streetName+"','"+postCode+"',"+"NOW());"
+              ,"hr", "thursday");
+
+      DBConnect.InsertQuery("insert into dept_emp (emp_no, dept_no, from_date, to_date) VALUES ("+employeeNumber+",'d010',NOW(),NOW()+10);", "hr", "thursday");
+
+      DBConnect.InsertQuery("insert into salaries (emp_no, salary, bank, AccountNumber, from_date, to_date) VALUES ("+employeeNumber+","+startingSalary+",'"+bankName+"','"+bankNum+"',NOW(),NOW()+10);", "hr", "thursday");
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void addSalesEmp(){
+
+    int commission;
+    int sales;
+    System.out.println("Forename: ");
+    forename = sc.nextLine();
+
+    System.out.println("Surname: ");
+    surname = sc.nextLine();
+
+    System.out.println("Street Name: ");
+    streetName = sc.nextLine();
+
+    System.out.println("House Number: ");
+    houseNumber = sc.nextLine();
+
+    System.out.println("Post Code: ");
+    postCode = sc.nextLine();
+
+    System.out.println("NIN: ");
+    NIN = sc.nextLine();
+
+    System.out.println("Bank Number: ");
+    bankNum = sc.nextLine();
+
+    System.out.println("Bank Name: ");
+    bankName = sc.nextLine();
+
+    System.out.println("Starting Salary (Do not include decimal point but include the values after the decimal point: ");
+    startingSalary = sc.nextInt();
+
+    System.out.println("Commission rate");
+    commission = sc.nextInt();
+
+    System.out.println("Total Sales");
+    sales = sc.nextInt();
 
     System.out.println("Employee Number: ");
     employeeNumber = sc.nextInt();
